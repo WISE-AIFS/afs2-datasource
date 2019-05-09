7730c0a5ab65fb9cc0c486be75492fea192f8934
stacy0416 <stac0416@hotmail.com>
2019-05-09 12:06:13 +0800

    [Changed] add license

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 setuptools.setup(
     name='afs2-datasource',
     version='1.0.11',
-    author='Stacy.yeh',
+    author='WISE-PaaS/AFS',
     author_email='stacy.yeh@advantech.com.tw',
     packages=setuptools.find_packages(),
     install_requires=[
@@ -14,7 +14,8 @@ setuptools.setup(
       "influxdb==5.2.2"
     ],
     description='For AFS developer to access Datasource',
-    keywords=['AFS']
+    keywords=['AFS'],
+    license='Apache License 2.0'
 )
 
 # python setup.py bdist_wheel
