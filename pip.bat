8e366576ab21288520edcf54766de5a174b7dc6a
stacy0416 <stac0416@hotmail.com>
2019-05-13 11:27:50 +0800

    [Changed] pypi setting

---
+++ pip.bat
@@ -0,0 +1,5 @@
+python setup.py sdist
+C:/Users/stacy.yeh/AppData/Local/Programs/Python/Python36/Scripts/twine.exe upload dist/*
+
+@ username: stacy.yeh
+@ password:
--- setup.py
+++ setup.py
@@ -1,21 +1,33 @@
 import os
 import setuptools
+try: # for pip >= 10
+  from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+  from pip.req import parse_requirements
+
+requirements_path = os.path.join(os.path.dirname(__file__), 'requirements.txt')
+install_requires = parse_requirements(requirements_path, session='hack')
+install_requires = [str(ir.req) for ir in install_requires]
+
+with open(os.path.join(os.path.dirname(__file__), 'VERSION'), 'r') as f:
+  version = f.read()
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r') as f:
+  long_description = f.read()
 
 setuptools.setup(
-    name='afs2-datasource',
-    version='1.0.11',
-    author='WISE-PaaS/AFS',
-    author_email='stacy.yeh@advantech.com.tw',
-    packages=setuptools.find_packages(),
-    install_requires=[
-      "pymongo==3.7.2",
-      "pandas==0.24.2",
-      "psycopg2-binary==2.8.1",
-      "influxdb==5.2.2"
-    ],
-    description='For AFS developer to access Datasource',
-    keywords=['AFS'],
-    license='Apache License 2.0'
+  name='afs2-datasource',
+  version=version,
+  description='For AFS developer to access Datasource',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  author='WISE-PaaS/AFS',
+  author_email='stacy.yeh@advantech.com.tw',
+  packages=setuptools.find_packages(),
+  install_requires=install_requires,
+  keywords=['AFS'],
+  license='Apache License 2.0',
+  url='https://github.com/stacy0416/afs2-datasource'
 )
 
 # python setup.py bdist_wheel
