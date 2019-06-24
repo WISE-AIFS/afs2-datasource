8360f7199b1db6d6b355783b1b7236e037bb4399
stacy0416 <stac0416@hotmail.com>
2019-06-24 17:03:08 +0800

    [Changed] add S3 test

--- test/Titanic.py
+++ test/Titanic.py
@@ -22,37 +22,43 @@ with open('config/influx_internal.json') as f:
   data = json.load(f)
   os.environ = data
 
-# with pd.read_csv('../dataTrain.csv') as csvfile:
-df = pd.read_csv('../dataTrain.csv')
-
 db = DBManager()
 db.connect()
-# create table
-columns = [
-  {'name': 'index', 'type': 'INTEGER'},
-  {'name': 'survived', 'type': 'FLOAT'},
-  {'name': 'age', 'type': 'FLOAT'},
-  {'name': 'embarked', 'type': 'INTEGER'},
-  {'name': 'fare', 'type': 'FLOAT'},
-  {'name': 'pclass', 'type': 'INTEGER'},
-  {'name': 'sex', 'type': 'INTEGER'},
-  {'name': 'title2', 'type': 'INTEGER'},
-  {'name': 'ticket_info', 'type': 'INTEGER'},
-  {'name': 'cabin', 'type': 'INTEGER'}
-]
 
-table_name = ''
-if db.get_dbtype() == constant.DB_TYPE['POSTGRES']:
-  table_name = 'afs.titanic'
-elif db.get_dbtype() == constant.DB_TYPE['MONGODB']:
-  table_name = 'titanic'
-elif db.get_dbtype() == constant.DB_TYPE['INFLUXDB']:
-  table_name = 'titanic'
+if db.get_dbtype() == constant.DB_TYPE['S3']:
+  bucket_name = 'test'
+  file_name = 'titanic.csv'
+  if not db.is_table_exist(bucket_name):
+    db.create_table(bucket_name)
+  db.insert(table_name=bucket_name, source='test/titanic.csv', destination=file_name)
+else:
+  df = pd.read_csv('test/titanic.csv')
+  # create table
+  columns = [
+    {'name': 'index', 'type': 'INTEGER'},
+    {'name': 'survived', 'type': 'FLOAT'},
+    {'name': 'age', 'type': 'FLOAT'},
+    {'name': 'embarked', 'type': 'INTEGER'},
+    {'name': 'fare', 'type': 'FLOAT'},
+    {'name': 'pclass', 'type': 'INTEGER'},
+    {'name': 'sex', 'type': 'INTEGER'},
+    {'name': 'title2', 'type': 'INTEGER'},
+    {'name': 'ticket_info', 'type': 'INTEGER'},
+    {'name': 'cabin', 'type': 'INTEGER'}
+  ]
 
-# if not db.is_table_exist(table_name):  
-db.create_table(table_name, columns)
-# insert records
-db.insert(table_name, list(df.columns.values), df.values)
+  table_name = ''
+  if db.get_dbtype() == constant.DB_TYPE['POSTGRES']:
+    table_name = 'afs.titanic'
+  elif db.get_dbtype() == constant.DB_TYPE['MONGODB']:
+    table_name = 'titanic'
+  elif db.get_dbtype() == constant.DB_TYPE['INFLUXDB']:
+    table_name = 'titanic'
 
-# close db
-db.disconnect()
+  # if not db.is_table_exist(table_name):  
+  db.create_table(table_name, columns)
+  # insert records
+  db.insert(table_name, list(df.columns.values), df.values)
+
+  # close db
+  db.disconnect()
